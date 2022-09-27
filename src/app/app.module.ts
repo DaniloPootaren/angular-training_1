752316d29a1b4c034cbe93be4cb6fd7aa7ddff28
Jean Danilo Maxwell Patrick Pootaren <62770019+Daniloxax@users.noreply.github.com>
2022-09-27 11:36:17 +0400

    added material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './shared/components/layout/layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { LayoutComponent } from './shared/components/layout/layout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
