5dba5693330151dcddde5c98f5d505653b67338a
Jean Danilo Maxwell Patrick Pootaren <62770019+Daniloxax@users.noreply.github.com>
2022-10-03 00:35:55 +0400

    Authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService as AuthGuard } from './core/authentication/auth-guard.service';
 import { CreateEditEventComponent } from './features/create-edit-event/create-edit-event.component';
 import { NotFoundComponent } from './features/not-found/not-found.component';
 import { ViewEventsComponent } from './features/view-events/view-events.component';
 
 const routes: Routes = [
-  {path:"", component: ViewEventsComponent},
-  {path:"create", component: CreateEditEventComponent},
-  {path:"edit/:id", component: CreateEditEventComponent},
-  {path:"**", component: NotFoundComponent},
+  { path: '', component: ViewEventsComponent, canActivate: [AuthGuard] },
+  {
+    path: 'create',
+    component: CreateEditEventComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'edit/:id',
+    component: CreateEditEventComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
